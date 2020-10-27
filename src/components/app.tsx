0050388f17a2c626f1e00b02307db9cfbc9e2019
Reed Anderson <reeda@ksu.edu>
2020-10-27 17:35:07 -0500

    Initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface IAppProps { }
+
+export default function App(props: IAppProps) {
+    return (
+        <h1>Hello React Typescript!</h1>
+    );
+}
